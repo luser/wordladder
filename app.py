b136856bdeceb37f005e24d741df3994cbca4897
Ted Mielczarek <ted.mielczarek@gmail.com>
2009-04-16 21:47:13 -0500

    basic JSON output for game and play, add polling to the game page to pick up other moves that were made and display them

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import web, pickle, sys, os, os.path
+import simplejson as json
 # not portable, but i don't care
 import fcntl
 
@@ -52,6 +53,22 @@ urls = (
 app = web.application(urls, globals())
 render = web.template.render('templates/')
 
+def wantsJSON():
+  #TODO: better Accept parsing?
+  return 'HTTP_ACCEPT' in web.ctx.environ and web.ctx.environ['HTTP_ACCEPT'].find('json') != -1
+
+def sendJSON(game, lastid, error=None):
+  web.header('Content-Type', 'text/plain')
+  j = {'moves': [], 'lastmove': game.lastmove}
+  for id in sorted(game.moves.keys()):
+    if id <= lastid:
+      continue
+    j['moves'].append({'id': id, 'word': game.moves[id].word,
+                       'parent': game.moves[id].parent.id})
+  if error:
+    j['error'] = error
+  return json.dumps(j)
+
 class index:
   def GET(self):
     return render.index(data.games)
@@ -70,25 +87,43 @@ class game:
     if not game in data.games:
       raise web.notfound()
     g = data.games[game]
-    return render.game(g)
+    if wantsJSON():
+      d = web.input(lastmove="1")
+      if d.lastmove.isdigit():
+        lastmove = int(d.lastmove)
+      else:
+        lastmove = 1
+      print >>sys.stderr, "lastmove: %d" % lastmove
+      return sendJSON(g, lastmove)
+    else:
+      return render.game(g)
 
 class play:
   def POST(self, game):
     if not game in data.games:
       raise web.notfound()
     g = data.games[game]
-    d = web.input(moveid=1, word=None)
+    d = web.input(moveid=1, word=None, lastmove=None)
     if not d.moveid.isdigit() or d.word is None:
-      #XXX: do something better
-      raise web.notfound()
+      raise web.badrequest()
     mid = int(d.moveid)
     if not mid in g.moves:
-      raise web.notfound()
+      raise web.badrequest()
+    json = wantsJSON()
+    if json:
+      if d.lastmove is None or not d.lastmove.isdigit():
+        raise web.badrequest()
     valid, reason = g.addmove(mid, d.word, 'user')
     if not valid:
-      return render.play(g, d.word, reason)
+      if not json:
+        return render.play(g, d.word, reason)
+      else:
+        return sendJSON(g, int(d.lastmove), reason)
     saveData(data)
-    raise web.seeother("/game/%s" % str(g))
+    if not json:
+      raise web.seeother("/game/%s" % str(g))
+    else:
+      return sendJSON(g, int(d.lastmove))
   
 if __name__ == "__main__":
   app.run()
--- static/game.js
+++ static/game.js
@@ -1,9 +1,52 @@
-$(document).ready(function()
+var debug = false;
+var timeout = -1;
+function handleSubmit(event)
+{
+  if (timeout != -1) {
+    //cancelTimeout(timeout);
+  }
+  //event.preventDefault();
+}
+
+function handleGameJSON(data)
+{
+  timeout = -1;
+  $.each(data.moves, function(i, m)
+         {
+           // m.id, m.word, m.parent
+           $('#m' + m.parent).parent().append('<li id="l'+m.id+'" style="display:none"><ul><span id="m'+m.id+'" class="move">'+m.word+'</span><form class="hidden" method="POST" action="'+window.location+'/play"><input type="hidden" name="moveid" value="'+m.id+'"><input type="text" name="word"></input></form></ul>');
+           $('#m' + m.id).attr('title', 'Click to add a word after this word').click(moveClick).next('form').submit(handleSubmit);
+           $('#l' + m.id).show('normal');
+         });
+  lastmove = data.lastmove;
+  // poll again later
+  if (!debug)
+    pollJSON();
+}
+
+function pollJSON()
+{
+  timeout = setTimeout(function() { $.getJSON(window.location + "?lastmove=" + lastmove, handleGameJSON); }, 2000);
+}
+
+function moveClick()
 {
   $('form').addClass('hidden');
+  $(this).next('form').removeClass('hidden').children('input').get(1).focus();
+}
+
+$(document).ready(function()
+{
+  $('form').addClass('hidden')
+    .submit(handleSubmit);
   $('.move').attr('title', 'Click to add a word after this word')
-    .click(function() {
-             $('form').addClass('hidden');
-             $(this).next('form').removeClass('hidden');
-           });
+    .click(moveClick);
+  // poll JSON
+  if (!debug) {
+    pollJSON();
+  }
+  else {
+    $(document.body).append('<button id="load">Load JSON</button>');
+    $('#load').click(pollJSON);
+  }
 });
