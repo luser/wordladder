17e7eb04be0c96239ceb08b6e54a2235797223b5
Ted Mielczarek <ted.mielczarek@gmail.com>
2009-04-20 10:28:34 -0400

    add script to remove a list of words from a dictionary

---
+++ mungedict.py
@@ -0,0 +1,20 @@
+#/usr/bin/env python
+# given a dictionary and a list of words, produce a reduced dictionary
+# with the list of words removed on stdout
+from __future__ import with_statement
+import sys
+from words import *
+
+def readwords(f):
+  with open(f, "r") as wordlist:
+    return [x.rstrip() for x in wordlist.readlines()]
+
+if len(sys.argv) != 3:
+  print >>sys.stderr, "usage: %s <dictionary> <word list>" % sys.argv[0]
+  sys.exit(1)
+
+dictwords = readwords(sys.argv[1])
+for w in readwords(sys.argv[2]):
+  if w in dictwords:
+    dictwords.remove(w)
+print "\n".join(dictwords)
