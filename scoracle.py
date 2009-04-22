05920fde9d5c8cfcb762c2912977c4dbad806857
Ted Mielczarek <ted.mielczarek@gmail.com>
2009-04-22 09:54:43 -0400

    first cut at a script to use the word matrix to fetch a min-path per word pair

---
+++ scoracle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+from __future__ import with_statement
+
+import sys, os, mmap, cgi
+from config import GAME_WORDLIST, DISTANCE_MATRIX
+
+def readwords(f):
+  with open(f, "r") as wordlist:
+    return [x.rstrip() for x in wordlist.readlines()]
+
+def minpath(start, end):
+  with open('wordmap', 'r') as f:
+    gamewordmap = dict((x, int(y)) for x,y in [l.rstrip().split(',') for l in f])
+  swid, ewid = gamewordmap[start], gamewordmap[end]
+  f = os.open(DISTANCE_MATRIX, os.O_RDWR)
+  m = mmap.mmap(f, len(gamewordmap) * len(gamewordmap))
+  return ord(m[swid * len(gamewordmap) + ewid])
+  m.close()
+  os.close(f)
+
+if __name__ == '__main__':
+  try:
+    form = cgi.FieldStorage()
+    if not (form.has_key("start") and form.has_key("end")):
+      raise Exception
+    start, end = form.getfirst("start"), form.getfirst("end")
+    print "Content-Type: text/plain\n"
+    print minpath(start, end)
+  except:
+    print "400 Bad Request\n"
